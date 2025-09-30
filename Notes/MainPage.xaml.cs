eceeccfb506e5ed51cc8d161824f8f77a7155b24
AbreuAngl <angelomello538@gmail.com>
2025-09-30 20:09:24 -0300

    Notes

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string caminho = Path.Combine(FileSystem.AppDataDirectory,"arquivo");
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if(File.Exists(caminho))
+                CaixaEditor.Text = File.ReadAllText(caminho);
+         
+        }
+
+        private void SalvarBtn_Clicked(object sender, EventArgs e)
+        {
+            String conteudo = CaixaEditor.Text;
+            File.WriteAllText(caminho, conteudo);
+            DisplayAlert("arquivo salvo", $"{Path.GetFileName(caminho)} salvo com sucesso em: {caminho}","ok");
+
+
+        }
+
+        private void ApagarBtn_Clicked(object sender, EventArgs e)
+        {
+            CaixaEditor.Text = String.Empty;
+            if (File.Exists(caminho))
+            {
+                File.Delete(caminho);
+                DisplayAlert("arquivo apagado", "arquivo apagado com sucesso", "ok");
+            }
+            else
+                DisplayAlert("exclusao", "arquivo nao existe", "ok");
+            
+        }
+    }
+
+}
